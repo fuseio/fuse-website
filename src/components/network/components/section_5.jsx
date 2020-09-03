4aaf1d3f098a32e7660a9f0fb21cbad2c5824d9a
Mulenga Bowa <mulengabowa53@gmail.com>
2020-09-04 01:38:58 +0200

    initial commit

--- src/components/network/components/section_5.jsx
+++ src/components/network/components/section_5.jsx
@@ -1,63 +1,12 @@
-import React, { useState } from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
-import { object, string } from "yup"
-
-const SignupSchema = object().shape({
-  email: string().email('Invalid email').required('Required'),
-})
+import React from 'react'
+import NewsletterForm from '../../shared/newsletter_form'
 
 const SectionFive = () => {
-  const [title, setTitle] = useState('')
   return (
     <section className="sign_for_updates grid-y align-middle align-center">
       <h1 className='title'>Sign up for updates</h1>
       <div className='text'>Don't miss the latest updates on Fuse and the <br /> future of payments.</div>
-      <Formik
-        initialValues={{ email: '' }}
-        validationSchema={SignupSchema}
-        onSubmit={async (values, { setSubmitting, resetForm }) => {
-          const { email } = values
-          const user = {
-            accountAddress: email,
-            email: email,
-            provider: 'HDWallet',
-            subscribe: true,
-            source: 'Fuse.io',
-            displayName: 'Fuse.io',
-          }
-          try {
-            const response = await fetch('https://studio.fuse.io/api/v2/users', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              referrerPolicy: 'no-referrer',
-              body: JSON.stringify(user)
-            })
-            if (response.status === 200) {
-              setTitle('<span>Thanks joining our mailing list! &#128077</span>')
-              setSubmitting(true)
-            } else if (response.status === 500) {
-              setTitle('<span>Something went wrong &#128078</span>')
-              setSubmitting(true)
-            }
-            resetForm({ email: '' })
-          } catch (error) {
-            resetForm({ email: '' })
-            setTitle('<span>Something went wrong &#128078</span>')
-            setSubmitting(true)
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className="form">
-            <div className="title" dangerouslySetInnerHTML={{ __html: title }} />
-            <Field type="email" className="form__input" placeholder="Enter email" name="email" />
-            <button disabled={isSubmitting} id="btn_submit" type="submit" className="form__button">Send</button>
-            <ErrorMessage name="email">{(msg) => <div className='error'>{msg}</div>}</ErrorMessage>
-          </Form>
-        )}
-      </Formik>
+      <NewsletterForm />
     </section>
   )
 }
---
+++ src/components/shared/newsletter_form.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { object, string } from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+const SignupSchema = object().shape({
+  email: string().email("Invalid email").required("Required"),
+});
+
+const NewsletterForm = () => {
+  const [title, setTitle] = useState("");
+
+  return (
+    <Formik
+      initialValues={{ email: "" }}
+      validationSchema={SignupSchema}
+      onSubmit={async (values, { setSubmitting, resetForm }) => {
+        const { email } = values;
+        const user = {
+          accountAddress: email,
+          email: email,
+          provider: "HDWallet",
+          subscribe: true,
+          source: "Fuse.io",
+          displayName: "Fuse.io",
+        };
+
+        try {
+          const response = await fetch("http://studio.fuse.io/api/v2/users", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(user),
+          });
+
+          if (response.status === 200) {
+            setTitle("<span>Thanks joining our mailing list! &#128077</span>");
+            setSubmitting(true);
+          } else if (response.status === 500) {
+            setTitle("<span>Something went wrong &#128078</span>");
+            setSubmitting(true);
+          }
+
+          resetForm({ email: "" });
+        } catch (error) {
+          resetForm({ email: "" });
+          setTitle("<span>Something went wrong &#128078</span>");
+          setSubmitting(true);
+        }
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form className="newsletter__form ">
+          <div className="title" dangerouslySetInnerHTML={{ __html: title }} />
+          <Field
+            type="email"
+            className="newsletter__form__input"
+            placeholder="Enter email"
+            name="email"
+          />
+          <ErrorMessage name="email">
+            {(msg) => (
+              <div className="newsletter__form__mobile-error">{msg}</div>
+            )}
+          </ErrorMessage>
+          <button
+            disabled={isSubmitting}
+            id="btn_submit"
+            type="submit"
+            className="newsletter__form__button"
+          >
+            Send
+          </button>
+          <ErrorMessage name="email">
+            {(msg) => <div className="newsletter__form__error ">{msg}</div>}
+          </ErrorMessage>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default NewsletterForm;
