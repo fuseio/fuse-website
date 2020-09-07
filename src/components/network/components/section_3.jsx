5f8018f4399e054459705c956405047d4ac42037
Lior Agnin <lior.agnin@gmail.com>
2020-09-07 10:06:58 +0300

    minor

--- src/components/network/components/section_3.jsx
+++ src/components/network/components/section_3.jsx
@@ -11,7 +11,7 @@ const SectionThree = () => {
   const { countUp: transactionCounter, start: transactionCounterStart } = useCountUp({
     start: 0,
     formattingFn: formatNumber,
-    end: 521993
+    end: 525170
   })
 
   const { countUp: communitiesCounter, start: communitiesCounterStart, update: updateCommunities } = useCountUp({
@@ -24,7 +24,7 @@ const SectionThree = () => {
   const { countUp: walletsCounter, start: walletsCounterStart } = useCountUp({
     start: 0,
     formattingFn: formatNumber,
-    end: 9683
+    end: 10005
   })
 
   return (
