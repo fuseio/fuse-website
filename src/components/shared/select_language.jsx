c6f5cb99da4303dcb90b233d6605d6d378d5d897
unknown <an30458@gmail.com>
2020-10-20 20:11:10 +0200

    fixing dropdown and edit hover

--- src/components/shared/select_language.jsx
+++ src/components/shared/select_language.jsx
@@ -1,42 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import classNames from 'classnames'
 import { useLang } from './lang_provider';
 
 const options = [
-  {label: "English", value: "en"},
-  {label: "中国酶", value: "zh"},
+  { label: "English", value: "en" },
+  { label: "中国酶", value: "zh" },
 ];
 
-const SelectLanguage = ({isNetworkHeader, isOpen}) => {
+const SelectLanguage = ({ isNetworkHeader, isOpen }) => {
   const [isListOpen, setIsOpen] = useState(false);
   const [lang, setLang] = useLang();
+  const containerRef = useRef(null);
+  const iconRef = useRef(null);
   const toggling = () => setIsOpen(!isListOpen);
 
   const onOptionClicked = ({ value }) => () => {
     setLang(value);
     setIsOpen(false);
   };
+  const handleClickOutside = (e) => {
+    if (isListOpen && containerRef.current && !iconRef.current.contains(e.target) && !containerRef.current.contains(e.target))
+      setIsOpen(false);
+  };
+
+  useEffect(() => {
+    if (isListOpen === true) {
+      document.addEventListener('mousedown', handleClickOutside)
+      return () => document.removeEventListener('mousedown', handleClickOutside);
+    }
+  }, [isListOpen])
 
   return (
-  <>
-    <a onClick={toggling} rel="noreferrer noopener" className={classNames('icon', { 'language': (!isNetworkHeader || isOpen), 'language--white': isNetworkHeader && !isOpen })} target='_blank'/>
-    {/* <div onClick={toggling} className='dropdown-header'>
-      {selectedOption.label}
-    </div> */}
-    <div className='dropdown-container'>
-      {isListOpen && (
-        <div className='dropdown-list-container'>
-          <ul className='dropdown-list'>
-            {options.map((option, index) => (
-              <li onClick={onOptionClicked(option)} key={index} className='list-item'>
-                {option.label}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  </>
+    <>
+      <a ref={iconRef} onClick={toggling} rel="noreferrer noopener" className={classNames('icon', { 'language': (!isNetworkHeader || isOpen), 'language--white': isNetworkHeader && !isOpen })} target='_blank' />
+      <div className='dropdown-container'>
+        {isListOpen && (
+          <div ref={containerRef} className='dropdown-list-container'>
+            <ul className='dropdown-list'>
+              {options.map((option, index) => (
+                <li onClick={onOptionClicked(option)} key={index} className='list-item'>
+                  {option.label}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
