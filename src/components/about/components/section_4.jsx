6f27a3b19542a1338c14ebdec6f0e22a405bdbfe
ahmed <an30458@gmail.com>
2020-12-28 18:20:26 +0200

    editing photos colors in about us page and the max-width

--- src/components/about/components/section_4.jsx
+++ src/components/about/components/section_4.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import aleph from '@/assets/images/aleph.png';
-import collider from '@/assets/images/collider.png';
-import spark from '@/assets/images/spark.png';
-import blackedge from '@/assets/images/blackedge.png';
-import defi from '@/assets/images/defi.png';
-import wings from '@/assets/images/wings.png';
-import trg from '@/assets/images/trg.png';
-import faculty from '@/assets/images/faculty.png';
-import techmeetstrader from '@/assets/images/techmeetstrader.png';
-import wolfedge from '@/assets/images/wolfedge.png';
-import block from '@/assets/images/block1.png';
-import sordex from '@/assets/images/sordex.png';
-import roost from '@/assets/images/roost.png';
-import portis from '@/assets/images/portis.png';
-import gooddollar from '@/assets/images/gooddollar.png';
-import fuseprime from '@/assets/images/fuseprime.png';
-import torus from '@/assets/images/torus.png';
-import ramp from '@/assets/images/ramp.png';
-import transac from '@/assets/images/transak.png';
-import monereum from '@/assets/images/monereum.png';
-import altcoin from '@/assets/images/altcoin.png';
-import crypto from '@/assets/images/crypto.png';
-import bitazza from '@/assets/images/bitazza.png';
-import primedao from '@/assets/images/primedao.png';
+import collider from '@/assets/images/collider.jpeg';
+import spark from '@/assets/images/spark.jpeg';
+import blackedge from '@/assets/images/blackedge.jpeg';
+import defi from '@/assets/images/defi.jpeg';
+import wings from '@/assets/images/wings.jpeg';
+import trg from '@/assets/images/trg.jpeg';
+import faculty from '@/assets/images/faculty.jpeg';
+import techmeetstrader from '@/assets/images/techmeetstrader.jpeg';
+import wolfedge from '@/assets/images/wolfedge.jpeg';
+import block from '@/assets/images/block.jpeg';
+import sordex from '@/assets/images/sordex.jpeg';
+import roost from '@/assets/images/roost.jpeg';
+import portis from '@/assets/images/portis.jpeg';
+import gooddollar from '@/assets/images/gooddollar.jpeg';
+import fuseprime from '@/assets/images/fuseprime.jpeg';
+import torus from '@/assets/images/torus.jpeg';
+import ramp from '@/assets/images/ramp.jpeg';
+import transac from '@/assets/images/transak.jpeg';
+import monereum from '@/assets/images/monereum.jpeg';
+import altcoin from '@/assets/images/altcoin.jpeg';
+import crypto from '@/assets/images/crypto.jpeg';
+import bitazza from '@/assets/images/bitazza.jpeg';
+import primedao from '@/assets/images/primedao.jpeg';
 const ImageCell = ({ image, styles }) => {
   return (
     <div className="section_four__image cell small-12 medium-6 grid-x align-middle align-center">
@@ -34,7 +34,7 @@ const ImageCell = ({ image, styles }) => {
 const SectionFour = () => {
   return (
     <section className="section_four">
-      <div className="section_four__container">
+      <div className="section_four__container grid-container">
         <div className="section_four__wrapper">
           <h2 className="title">Partners</h2>
           <div className="logos cell large-auto grid-x ">
