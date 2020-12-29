9728b33ac9b94c6cd189c7f660ea6420ece674f8
ahmed <an30458@gmail.com>
2020-12-29 18:47:50 +0200

    editing photos

--- src/components/home/components/section_6.jsx
+++ src/components/home/components/section_6.jsx
@@ -1,58 +1,59 @@
-import React from 'react'
-import { FormattedMessage } from 'react-intl'
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
 
 const SectionSix = (props) => {
   return (
-    <section className='why_fuse_better'>
-      <div className='why_fuse_better__wrapper grid-x align-middle'>
-        <div className='why_fuse_better__content'>
+    <section className="why_fuse_better">
+      <div className="why_fuse_better__wrapper grid-x align-middle">
+        <div className="why_fuse_better__content">
           <div>
-            <h1 className='title'>
+            <h1 className="title">
               <FormattedMessage
-                defaultMessage='Why open {newLine} source{newLine} payments on{newLine} <span>Fuse</span> are A {newLine} better cash{newLine} Alternative?'
+                defaultMessage="Why open {newLine} source{newLine} payments on{newLine} <span>Fuse</span> are A {newLine} better cash{newLine} Alternative?"
                 values={{
                   newLine: <br />,
-                  span: chunks => <span>{chunks}</span>
+                  span: (chunks) => <span>{chunks}</span>,
                 }}
               />
             </h1>
           </div>
-          <div className='items'>
-            <div className='item'>
-              <div className='image'>
-                <img src='./images/network_world.png' alt='network_world' />
+          <div className="items">
+            <div className="item">
+              <div className="image">
+                <img src="./images/network_world.png" alt="network_world" />
               </div>
-              <div className='line' />
-              <div className='text'>
-                <FormattedMessage defaultMessage='It has a very low entry barrier so this technology can reach anywhere across the world' />
-
-              </div>
-            </div>
-            <div className='item'>
-              <div className='image'>
-                <img src='./images/network_wallet.png' alt='network_wallet' />
-              </div>
-              <div className='line' />
-              <div className='text'>
-                <FormattedMessage defaultMessage='Connect to decentralized finance so you have more things to do with your money' />
+              <div className="line" />
+              <div className="text">
+                <FormattedMessage defaultMessage="It has a very low entry barrier so this technology can reach anywhere across the world" />
               </div>
             </div>
-            <div className='item'>
-              <div className='image grid-x align-center align-middle'>
-                <img style={{ width: '75px', height: '75px' }} src='./images/network_coin.png' alt='network_coin.png' />
+            <div className="item">
+              <div className="image">
+                <img src="./images/network_wallet.png" alt="network_wallet" />
               </div>
-              <div className='line' />
-              <div className='text'>
-                <FormattedMessage
-                  defaultMessage='Pay a fixed fee of up to 1 cent per transaction to move money instantly without any hidden fees and inefficiencies'
+              <div className="line" />
+              <div className="text">
+                <FormattedMessage defaultMessage="Connect to decentralized finance so you have more things to do with your money" />
+              </div>
+            </div>
+            <div className="item">
+              <div className="image grid-x align-center align-middle">
+                <img
+                  style={{ width: '75px', height: '75px' }}
+                  src="./images/network_coin.png"
+                  alt="network_coin.png"
                 />
+              </div>
+              <div className="line" />
+              <div className="text">
+                <FormattedMessage defaultMessage="Pay a fixed fee of up to 1 cent per transaction to move money instantly without any hidden fees and inefficiencies" />
               </div>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SectionSix
+export default SectionSix;
