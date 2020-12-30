8e1565ea55f0976f133fd5d1c9f412830fbe5016
Lior Agnin <lior.agnin@gmail.com>
2020-12-30 12:58:59 +0200

    linter

--- src/components/about/components/section_4.jsx
+++ src/components/about/components/section_4.jsx
@@ -1,43 +1,44 @@
-import React from 'react';
-import collider from '@/assets/images/collider.svg';
-import spark from '@/assets/images/spark.svg';
-import blackedge from '@/assets/images/blackedge.png';
-import defi from '@/assets/images/defi.svg';
-import wings from '@/assets/images/wings.svg';
-import trg from '@/assets/images/trg.png';
-import faculty from '@/assets/images/faculty.svg';
-import techmeetstrader from '@/assets/images/techmeetstrader.png';
-import wolfedge from '@/assets/images/wolfedge.svg';
-import block from '@/assets/images/block.png';
-import sordex from '@/assets/images/sordex.png';
-import roost from '@/assets/images/roost.png';
-import portis from '@/assets/images/portis.svg';
-import gooddollar from '@/assets/images/gooddollar.png';
-import fuseprime from '@/assets/images/fuseprime.svg';
-import torus from '@/assets/images/torus.svg';
-import ramp from '@/assets/images/ramp.svg';
-import transac from '@/assets/images/transac.svg';
-import monereum from '@/assets/images/monereum.svg';
-import altcoin from '@/assets/images/altcoin.png';
-import crypto from '@/assets/images/crypto.png';
-import bitazza from '@/assets/images/bitazza.svg';
-import primedao from '@/assets/images/primedao.svg';
+import React from 'react'
+import collider from '@/assets/images/collider.svg'
+import spark from '@/assets/images/spark.svg'
+import blackedge from '@/assets/images/blackedge.png'
+import defi from '@/assets/images/defi.svg'
+import wings from '@/assets/images/wings.svg'
+import trg from '@/assets/images/trg.png'
+import faculty from '@/assets/images/faculty.svg'
+import techmeetstrader from '@/assets/images/techmeetstrader.png'
+import wolfedge from '@/assets/images/wolfedge.svg'
+import block from '@/assets/images/block.png'
+import sordex from '@/assets/images/sordex.png'
+import roost from '@/assets/images/roost.png'
+import portis from '@/assets/images/portis.svg'
+import gooddollar from '@/assets/images/gooddollar.png'
+import fuseprime from '@/assets/images/fuseprime.svg'
+import torus from '@/assets/images/torus.svg'
+import ramp from '@/assets/images/ramp.svg'
+import transac from '@/assets/images/transac.svg'
+import monereum from '@/assets/images/monereum.svg'
+import altcoin from '@/assets/images/altcoin.png'
+import crypto from '@/assets/images/crypto.png'
+import bitazza from '@/assets/images/bitazza.svg'
+import primedao from '@/assets/images/primedao.svg'
+
 const ImageCell = ({ image, styles }) => {
   return (
-    <div className="section_four__image cell small-12 medium-6 grid-x align-middle align-center">
+    <div className='section_four__image cell small-12 medium-6 grid-x align-middle align-center'>
       <img src={image} style={{ ...styles }} />
     </div>
-  );
-};
+  )
+}
 
 const SectionFour = () => {
   return (
-    <section className="section_four">
-      <div className="section_four__container grid-container">
-        <div className="section_four__wrapper">
-          <h2 className="title">Partners</h2>
-          <div className="logos cell large-auto grid-x ">
-            <div className="logos__overlay"></div>
+    <section className='section_four'>
+      <div className='section_four__container grid-container'>
+        <div className='section_four__wrapper'>
+          <h2 className='title'>Partners</h2>
+          <div className='logos cell large-auto grid-x '>
+            <div className='logos__overlay' />
             <ImageCell image={gooddollar} />
             <ImageCell image={roost} />
             <ImageCell image={defi} />
@@ -60,10 +61,10 @@ const SectionFour = () => {
           </div>
         </div>
         <hr />
-        <div className="section_four__wrapper">
-          <h2 className="title">Early Backers</h2>
-          <div className="logos cell large-auto grid-x ">
-            <div className="logos__overlay"></div>
+        <div className='section_four__wrapper'>
+          <h2 className='title'>Early Backers</h2>
+          <div className='logos cell large-auto grid-x '>
+            <div className='logos__overlay' />
             <ImageCell image={collider} />
             <ImageCell image={wolfedge} />
             <ImageCell image={trg} />
@@ -78,7 +79,7 @@ const SectionFour = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default SectionFour;
+export default SectionFour
--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import SectionOne from './components/section_1';
-import SectionTwo from './components/section_2';
-import SectionThree from './components/section_3';
-import SectionFour from './components/section_4';
-import ContactUs from '@/components/shared/ContactUs';
-import Footer from '@/components/footer';
+import React from 'react'
+import SectionOne from './components/section_1'
+import SectionTwo from './components/section_2'
+import SectionThree from './components/section_3'
+import SectionFour from './components/section_4'
+import ContactUs from '@/components/shared/ContactUs'
+import Footer from '@/components/footer'
 
 const About = () => {
   return (
@@ -16,7 +16,7 @@ const About = () => {
       <ContactUs />
       <Footer isNetworkFooter />
     </>
-  );
-};
+  )
+}
 
-export default About;
+export default About
