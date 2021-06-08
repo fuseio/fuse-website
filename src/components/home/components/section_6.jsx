a1496e5998b7f7ef22c26ad97c6acb2beeb508c6
ahmed <an30458@gmail.com>
2021-06-08 13:37:18 +0200

    adding links to logos

--- src/components/home/components/section_6.jsx
+++ src/components/home/components/section_6.jsx
@@ -36,6 +36,214 @@ import pancake from '@/assets/img/Ecosystem/pancake-swap_partner.png'
 import krystal from '@/assets/img/Ecosystem/krystal_partner.png'
 import immunefi from '@/assets/img/Ecosystem/immunefi_partner.png'
 import getblock from '@/assets/img/Ecosystem/getblock_partner.png'
+
+const largeEcoSystem = [
+  {
+    logo: ola,
+    link:'https://ola.finance/',
+    alt:'ola'
+  },
+  {
+    logo: graph,
+    link:'https://thegraph.com/',
+    alt:'graph'
+  },
+  {
+    logo: ramp,
+    link:'https://ramp.network/',
+    alt:'ramp'
+  },
+  {
+    logo: pokt,
+    link:'https://www.pokt.network/',
+    alt:'pokt'
+  },
+  {
+    logo: zokyo,
+    link:'https://www.zokyo.io/',
+    alt:'zokyo'
+  },
+  {
+    logo: zeppelin,
+    link:'https://openzeppelin.com/',
+    alt:'zepplin'
+  },
+  {
+    logo: monerium,
+    link:'https://monerium.com/',
+    alt:'monerium'
+  },
+  {
+    logo: staking,
+    link:'https://www.stakingrewards.com',
+    alt:'staking'
+  },
+  {
+    logo: chainlink,
+    link:'https://chain.link/',
+    alt:'chainlink'
+  },
+  {
+    logo: goodfi,
+    link:'https://www.goodfi.com/',
+    alt:'goodfi'
+  },
+  {
+    logo: emoney,
+    link:'https://e-money.com/',
+    alt:'emoney'
+  },
+  {
+    logo: dapp,
+    link:'https://www.dapp.com/',
+    alt:'dapp'
+  }
+]
+
+const smallEcoSystem = [
+  {
+    logo: coingecko,
+    link:'https://www.coingecko.com/',
+    alt:'coingecko'
+  },
+  {
+    logo: defilama,
+    link:'https://defillama.com/home',
+    alt:'defilama'
+  },
+  {
+    logo: coinmarketcap,
+    link:'https://coinmarketcap.com/',
+    alt:'coinmarketcap'
+  },
+  {
+    logo: uniswap,
+    link:'https://uniswap.org/',
+    alt:'uniswap'
+  },
+  {
+    logo: kyber,
+    link:'https://kyber.network/',
+    alt:'kyber'
+  },
+  {
+    logo: oneinch,
+    link:'https://1inch.io/',
+    alt:'oneinch'
+  },
+  {
+    logo: loopring,
+    link:'https://loopring.org/',
+    alt:'loopring'
+  },
+  {
+    logo: defipulse,
+    link:'https://defipulse.com/',
+    alt:'defipulse'
+  },
+  {
+    logo: zerochain,
+    link:'https://0chain.net/',
+    alt:'zerochain'
+  },
+  {
+    logo: twokey,
+    link:'https://www.2key.network/',
+    alt:'twokey'
+  },
+  {
+    logo: mantradao,
+    link:'https://mantradao.com/',
+    alt:'mantradao'
+  },
+  {
+    logo: torus,
+    link:'https://tor.us/',
+    alt:'torus'
+  },
+  {
+    logo: transak,
+    link:'https://transak.com/',
+    alt:'transak'
+  },
+  {
+    logo: unstoppabledomains,
+    link:'https://unstoppabledomains.com/',
+    alt:'unstoppabledomains'
+  },
+  {
+    logo: orion,
+    link:'https://www.orionprotocol.io/',
+    alt:'orion'
+  },
+  {
+    logo: boson,
+    link:'https://www.bosonprotocol.io/',
+    alt:'boson'
+  },
+  {
+    logo: unmarshal,
+    link:'https://unmarshal.io/',
+    alt:'unmarshal'
+  },
+  {
+    logo: dappradar,
+    link:'https://dappradar.com/',
+    alt:'dappradar'
+  },
+  {
+    logo: defistation,
+    link:'https://www.defistation.io/',
+    alt:'defistation'
+  },
+  {
+    logo: mintgate,
+    link:'https://mintgate.app/',
+    alt:'mintgate'
+  },
+  {
+    logo: pancake,
+    link:'https://pancakeswap.finance/',
+    alt:'pancake'
+  },
+  {
+    logo: krystal,
+    link:'https://krystal.app/',
+    alt:'krystal'
+  },
+  {
+    logo: immunefi,
+    link:'https://immunefi.com/',
+    alt:'immunefi'
+  },
+  {
+    logo: getblock,
+    link:'http://getblock.io/',
+    alt:'getblock'
+  },
+]
+
+const LargeCompanyLogo = ({ logo, link, alt }) => {
+  return (
+    <div className='fuse_ecosysyem__logo-priority small-12 medium-6  large-4'>
+       <a href={link} rel='noreferrer noopener' target='_blank'>   
+        <img src={logo} alt= {`${alt}`}/>
+       </a> 
+    </div>
+  )
+}
+
+const SmallCompanyLogo = ({ logo, link, alt }) =>{
+  return(
+    <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
+      <a href={link} rel='noreferrer noopener' target='_blank'>
+       <img src={logo} alt= {`${alt}`}/>
+      </a>
+    </div>
+  )
+}
+
+
 const FuseEcosystem = (props) => {
   return (
     <section className='fuse_ecosysyem'>
@@ -46,126 +254,18 @@ liquidity platforms, data providers and more!' />
       <div className='fuse_ecosysyem__items grid-x align-justify'>
         <div className='grid-x fuse_ecosysyem__logo-block cell'>
           <div className='grid-x fuse_ecosysyem__logo-row'>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6  large-4'>
-              <img src={ola} alt='ola' />
-            </div>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6 large-4'>
-              <img src={graph} alt='graph' />
-            </div>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6 large-4'>
-             <img src={ramp} alt='ramp' />
-            </div>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6 large-4'>
-              <img src={pokt} alt='pokt' />
-            </div>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6 large-4'>
-              <img src={zokyo} alt='zokyo' />
-            </div>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6 large-4'>
-              <img src={zeppelin} alt='zeppelin' />
-            </div>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6 large-4'>
-              <img src={monerium} alt='monerium' />
-            </div>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6 large-4'>
-              <img src={staking} alt='staking' />
-            </div>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6 large-4'>
-             <img src={chainlink} alt='chainlink' />
-            </div>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6 large-4'>
-              <img src={goodfi} alt='goodfi' />
-            </div>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6 large-4'>
-              <img src={emoney} alt='emoney' />
-            </div>
-            <div className='fuse_ecosysyem__logo-priority small-12 medium-6 large-4'>
-              <img src={dapp} alt='dapp' />
-            </div>
-          </div>
-          {/* <div className='grid-x fuse_ecosysyem__logo-row'>
-          </div> */}
+             {
+              largeEcoSystem.map((logo, index) => <LargeCompanyLogo key={index} {...logo} />)
+             }
+          </div>  
         </div>
         <div className='grid-x fuse_ecosysyem__logo-block cell'>
           <div className='grid-x fuse_ecosysyem__logo-row'>
-           <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={coingecko} alt='coingecko' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={defilama} alt='defilama' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-             <img src={coinmarketcap} alt='coinmarketcap' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={uniswap} alt='uniswap' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={kyber} alt='kyber' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={oneinch} alt='oneinch' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={loopring} alt='loopring' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={defipulse} alt='defipulse' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={zerochain} alt='zerochain' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={twokey} alt='twokey' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={mantradao} alt='mantradao' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={torus} alt='torus' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={transak} alt='transak' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={unstoppabledomains} alt='unstoppabledomains' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-             <img src={orion} alt='orion' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={boson} alt='boson' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={unmarshal} alt='unmarshal' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={dappradar} alt='dappradar' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={defistation} alt='defistation' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={mintgate} alt='mintgate' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={pancake} alt='pancake' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={krystal} alt='krystal' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={immunefi} alt='immunefi' />
-            </div>
-            <div className='fuse_ecosysyem__logo-small small-6 medium-4 large-2'>
-              <img src={getblock} alt='getblock' />
-            </div>
+          {
+              smallEcoSystem.map((logo, index) => <SmallCompanyLogo key={index} {...logo} />)
+             }
           </div>
         </div>
-        {/* <div className='grid-x fuse_ecosysyem__logo-block cell'>
-          <div className='grid-x fuse_ecosysyem__logo-row'>
-          </div>
-        </div> */}
       </div>
     </section>
   )
