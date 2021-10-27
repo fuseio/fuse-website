ce03129b500d583ae9fc7e01aa8bab5494a6018a
Lior Agnin <lior.agnin@gmail.com>
2021-10-27 11:53:10 +0300

    linter

--- src/components/home/components/section_1.jsx
+++ src/components/home/components/section_1.jsx
@@ -4,15 +4,15 @@ import Globe from './globe'
 
 const msgs = defineMessages({
   primaryText: {
-    defaultMessage: 'Launch Community',
+    defaultMessage: 'Launch Community'
   },
   secondaryText: {
-    defaultMessage: 'Stake Fuse',
+    defaultMessage: 'Stake Fuse'
   }
 })
 
-const Button = ({ link, type = "primary", text }) => {
-  const className = (type === "primary") ? "main_button main_button--no-arrow main_button--bold" : "secondary_button";
+const Button = ({ link, type = 'primary', text }) => {
+  const className = (type === 'primary') ? 'main_button main_button--no-arrow main_button--bold' : 'secondary_button'
   return (
     <a
       rel='noreferrer noopener' target='_blank'
@@ -42,9 +42,9 @@ const SectionOne = () => {
                 }}
               />
             </p>
-            <div className="buttons">
-              <Button link='https://studio.fuse.io/' type="primary" text={msgs.primaryText} />
-              <Button link='https://staking.fuse.io/' type="secondary" text={msgs.secondaryText} />
+            <div className='buttons'>
+              <Button link='https://studio.fuse.io/' type='primary' text={msgs.primaryText} />
+              <Button link='https://staking.fuse.io/' type='secondary' text={msgs.secondaryText} />
             </div>
           </div>
           <div className='canvas grid-x align-right'>
